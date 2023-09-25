47490b952abdc5e078e2d52846cdf7504288358f
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2023-09-26 00:09:34 +0200

    add products models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    subtitle = models.TextField(max_length=500)
+    description = models.TextField(max_length=100000)
+    image = models.ImageField(upload_to='products')
+    price = models.FloatField()
+    flag = models.CharField(max_length=10)
+    brand = ''
+    sku = models.CharField(max_length=12)
+    quantity = models.IntegerField()
+
