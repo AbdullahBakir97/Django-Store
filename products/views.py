9ccffbf1e139729fa1c6eb6631d3e12389c3dbe7
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2023-10-04 21:59:31 +0200

    edit views and add templets file

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
-# Create your views here.
+from .models import Product , Brand , Review
+
+
+class ProductList(generic.ListView):
+    model = Product
+
+
+
+class ProductDetail(generic.DetailView):
+    model = Product
