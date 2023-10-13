6bd6b0aa6f1ff479caa72290d5254521ca416bc7
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2023-10-13 20:26:31 +0200

    add brand list

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import ProductList , ProductDetail
+from .views import ProductList , ProductDetail , BrandList
 
 
 
 urlpatterns = [
     path('',ProductList.as_view()) ,
     path('<slug:slug>',ProductList.as_view()) ,
+
+    path('brands/' , BrandList.as_view()),
 ]
+
--- products/views.py
+++ products/views.py
@@ -10,3 +10,7 @@ class ProductList(generic.ListView):
 
 class ProductDetail(generic.DetailView):
     model = Product
+
+
+class BrandList(generic.ListView):
+    model = Brand
