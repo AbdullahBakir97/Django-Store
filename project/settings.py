256bee5b050761274d376248a4df06b2aa6473ee
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2023-10-30 15:59:13 +0100

    add pagnation

--- project/settings.py
+++ project/settings.py
@@ -48,6 +48,12 @@ INSTALLED_APPS = [
     'products',
 ]
 
+# restapi settings
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
