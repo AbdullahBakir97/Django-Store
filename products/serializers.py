ead558e1886f0719c2ea26fc7ef2bd53047f83f7
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2023-10-30 18:43:02 +0100

    show Forignkey as string in api

--- products/serializers.py
+++ products/serializers.py
@@ -3,7 +3,13 @@ from rest_framework import serializers
 from .models import Product , Brand , Review , ProductImages
 
 
+class BrandSerializer(serializers.ModelSerializer):
+     class Meta:
+        model = Brand
+        fields = '__all__'
+
 class ProductSerializer(serializers.ModelSerializer):
+    brand = BrandSerializer()
     class Meta:
         model = Product
         fields = '__all__'
@@ -11,7 +17,3 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 
-class BrandSerializer(serializers.ModelSerializer):
-     class Meta:
-        model = Brand
-        fields = '__all__'
