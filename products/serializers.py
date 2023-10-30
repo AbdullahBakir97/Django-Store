5de04e8a3f6b4a2176562bc03fd9e52e199c8c71
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2023-10-30 20:24:27 +0100

    show avg rate in professional  mode

--- products/serializers.py
+++ products/serializers.py
@@ -9,55 +9,65 @@ class BrandSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ProductListSerializer(serializers.ModelSerializer):
-    brand = BrandSerializer()
+    brand = serializers.StringRelatedField()
     review_count = serializers.SerializerMethodField()
     avg_rate = serializers.SerializerMethodField()
+    avg_rate2 = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields = '__all__'
-
+        
     def get_review_count(self,object):
         review_count = object.product_review.all().count()
         return review_count
-
-
+        
+        
     def get_avg_rate(self,object):
         total = 0
         reviews = object.product_review.all()
         for r in reviews:
             total += r.rate
-
-        if len(reviews) > 0:
+            
+        if len(reviews) > 0: 
             return round(total/len(reviews),2)
         else:
             return 0
+
+    def get_avg_rate2(self,object):
+        avg = object.product_review.aggregate(rate_avg=Avg('rate'))
+        return round(avg['rate_avg'],2) if avg['rate_avg'] else 0 
+
+
+
+
         
-
-
-
-def get_avg_rate2(self,object):
-    avg = object.product_review.aggregate(rate_avg=Avg('rate'))
-    return round(avg['rate_avg'],2) if avg ['rate_avg'] else 0
-
-
+        
 class ProductDetailSerializer(serializers.ModelSerializer):
-    brand = BrandSerializer()
+    brand = serializers.StringRelatedField()
+    review_count = serializers.SerializerMethodField()
+    avg_rate = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields = '__all__'
+        
 
-
-
-
+    def get_review_count(self,object):
+        review_count = object.product_review.all().count()
+        return review_count
+            
+    def get_avg_rate(self,object):
+        avg = object.product_review.aggregate(rate_avg=Avg('rate'))
+        return round(avg['rate_avg'],2) if avg['rate_avg'] else 0 
+        
+        
+        
 class BrandListSerializer(serializers.ModelSerializer):
-     class Meta:
+    class Meta:
         model = Brand
         fields = '__all__'
-
-
-
-
+        
+        
 class BrandDetailSerializer(serializers.ModelSerializer):
-     class Meta:
+    class Meta:
         model = Brand
         fields = '__all__'
