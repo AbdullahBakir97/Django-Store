98096b78e3b9c2a3b6f2666c9d4e4c6eb4e87e29
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2023-11-20 19:57:13 +0100

    add cart & cart detail models

--- orders/models.py
+++ orders/models.py
@@ -34,6 +34,26 @@ class OrderDetail(models.Model):
     total = models.FloatField()
 
 
+CART_STATUS = (
+    ('Inprogress','Inprogress'),
+    ('Completed','Completed'),
+)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User,related_name='cart_user',on_delete=models.SET_NULL , null=True,blank=True)
+    status = models.CharField(max_length=15 , choices=CART_STATUS,default='Recieved')
+    coupon = models.ForeignKey('Coupon',related_name='order_coupon',on_delete=models.SET_NULL , null=True,blank=True)
+    cart_total_discount = models.FloatField(null=True,blank=True)
+
+    def __str__(self):
+        return str(self.user)
+
+class CartDetail(models.Model):
+    cart = models.ForeignKey(Cart,related_name='cart_detail',on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,related_name='cartdetail_product',on_delete=models.SET_NULL , null=True,blank=True)
+    quantity = models.IntegerField()
+    total = models.FloatField()
+
 
 class Coupon(models.Model):
     code = models.CharField(max_length=15)
