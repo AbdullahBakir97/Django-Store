26e6d644629de6b54c519eff8e4c368d7f90fec0
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2023-11-26 23:29:40 +0100

    show orders list

--- accounts/models.py
+++ accounts/models.py
@@ -15,3 +15,7 @@ class DeliveryAddress(models.Model):
     user = models.ForeignKey(User,related_name='user_address',on_delete=models.CASCADE)
     type = models.CharField(max_length=20,choices=ADDRESS_TYPE)
     address = models.TextField(max_length=300)
+
+
+    def __str__(self):
+        return f"{self.type} - {self.address}"
