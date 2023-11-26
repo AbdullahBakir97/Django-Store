aa2126614307ef4b6e67cf6d10d901d0dcf4a6a8
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2023-11-26 04:12:45 +0100

    add views structure for orders

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import add_to_cart
+from .views import add_to_cart , order_list , checkout , process_payment , payment_success , payment_failed
 
 app_name = 'orders'
 
 
 
 urlpatterns = [
-    path('add-to-cart' , add_to_cart , name='add-to-cart')
+    path('',order_list),
+    path('checkout' , checkout),
+    path('checkout/payment' , process_payment),
+    path('checkout/payment/success' , payment_success),
+    path('checkout/payment/failed' , payment_failed),
+    path('add-to-cart' , add_to_cart , name='add-to-cart'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render , redirect
-from .models import Cart , CartDetail
+from .models import Cart , CartDetail , Order
 from products.models import Product
+
+
+
+def order_list(request):
+    orders = Order.objects.all()
+    return render(request,'orders/orderlist.html',{'orders':orders})
+
 
 
 
@@ -17,3 +24,24 @@ def add_to_cart(request):
     cart_detail.save()
 
     return redirect(f"/products/{product.slug}")
+
+
+
+def checkout(request):
+    checkout = ''
+    return render(request, 'orders/checkout.html',{})
+
+
+
+def process_payment(request):
+    pass
+
+
+
+def payment_success(request):
+    pass
+
+
+
+def payment_failed(request):
+    pass
