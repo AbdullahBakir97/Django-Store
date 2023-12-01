3bb534acc592f13c940f16f2f053a7545636394f
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2023-12-01 20:26:55 +0100

    add order list api

---
+++ orders/api.py
@@ -0,0 +1,20 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+import datetime
+
+from .models import Cart , CartDetail , Order , OrderDetail 
+from .serializers import CartSerializer , CartDetailSerializer , OrderSerializer , OrderDetailSerializer
+from settings.models import DeliveryFee
+
+
+class OrderListAPI(generics.ListAPIView):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+
+    # get_queryset , list
+    def list(self, request, *args, **kwargs):
+        user = User.objects.get(username=self.kwargs['username'])
+        queryset = self.get_queryset().filter(user=user)  
+        data = OrderSerializer(queryset,many=True).data
+        return Response({'orders':data})
--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import add_to_cart , order_list , checkout , process_payment , payment_success , payment_failed
 
+from .api import OrderListAPI
+
 app_name = 'orders'
 
 
@@ -12,4 +14,9 @@ urlpatterns = [
     path('checkout/payment/success' , payment_success),
     path('checkout/payment/failed' , payment_failed),
     path('add-to-cart' , add_to_cart , name='add-to-cart'),
+
+
+    # api
+    path('api/<str:username>', OrderListAPI.as_view()),
+
 ]
