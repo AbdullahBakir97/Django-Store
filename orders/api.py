66a91181f434b6ed7434ad1bc5ad520a1d022536
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2023-12-01 21:10:33 +0100

    add apply coupon API

--- orders/api.py
+++ orders/api.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User
 import datetime
 
-from .models import Cart , CartDetail , Order , OrderDetail 
+from .models import Cart , CartDetail , Order , OrderDetail , Coupon
 from .serializers import CartSerializer , CartDetailSerializer , OrderSerializer , OrderDetailSerializer
 from settings.models import DeliveryFee
 
@@ -24,3 +24,32 @@ class OrderListAPI(generics.ListAPIView):
 class OrderDetailAPI(generics.RetrieveAPIView):
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
+
+
+class ApplyCouponAPI(generics.GenericAPIView):
+
+    def post(self, request, *args, **kwargs):
+        user = User.objects.get(username=self.kwargs['username'])
+        cart = Cart.objects.get(user=request.user , status='Inprogress')
+        cart_detail = CartDetail.objects.filter(cart=cart)
+        delivery_fee = DeliveryFee.objects.last().fee
+        sub_total = cart.cart_total()
+        coupon = Coupon.objects.get(code=request.data['coupon_code'])
+
+        if coupon and coupon.quantity > 0 :
+            today = datetime.datetime.today().date()
+            if today >= coupon.start_date and today < coupon.end_date :
+                coupon_value = sub_total / 100*coupon.discount
+                sub_total = sub_total - coupon_value
+                total = sub_total + delivery_fee
+
+                cart.coupon = coupon
+                cart.cart_total_discount = sub_total
+                cart.save()
+
+                return Response({'message':'coupon code applied successfully'})
+            else:
+                return Response({'message':'coupon code date is not valid '})
+            
+        
+        return Response({'message':'coupon not found or coupon ended '})
