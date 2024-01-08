fa0f3207815aeef1b1dc78c7fe42dbf52803c946
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-01-08 18:15:20 +0100

    add gmail settings to send emails

--- project/settings.py
+++ project/settings.py
@@ -202,4 +202,11 @@ LANGUAGES = [
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'   # service
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "abdullah.bakir.204@gmail.com"
+EMAIL_HOST_PASSWORD = "txhydnysjpckaikz"
